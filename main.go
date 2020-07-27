2e6a7fdf985672b911cfaaaf2854f1ecf8d10387
윤혁진scott.hj <scott.hj@kakaomobility.com>
2020-07-27 17:06:04 +0900

    call heroku

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/parnurzeal/gorequest"
 )
 
 func main() {
 	fmt.Println("Hello World!")
+
+	request := gorequest.New()
+	resp, _, _ := request.Get("https://cheoeum.herokuapp.com").End()
+	fmt.Print("Call heroku status code: ")
+	fmt.Println(resp.StatusCode)
 }
