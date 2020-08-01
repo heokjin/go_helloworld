ec4e6b3a8b580084e78ab1508e19066debcef360
윤혁진scott.hj <scott.hj@kakaomobility.com>
2020-08-01 15:00:02 +0900

    add kingofweather-api check url

--- main.go
+++ main.go
@@ -6,10 +6,16 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println("Hello.. Hit the Server!")
 
 	request := gorequest.New()
 	resp, _, _ := request.Get("https://cheoeum.herokuapp.com").End()
 	fmt.Print("Call heroku status code: ")
 	fmt.Println(resp.StatusCode)
+
+	resp, _, _ = request.Get("https://kingofweather-api.herokuapp.com/check").End()
+	fmt.Print("Call KingOfWeather-Api status code: ")
+	fmt.Println(resp.StatusCode)
+
+
 }
